faaffc7d9ac6248b722c6dd469066c32916f5643
Camille Coti <coti@maximum.lipn.univ-paris13.fr>
2012-11-27 03:06:03 +0100

    creation de la branche PARrfoo

--- bin/par.sh
+++ bin/par.sh
@@ -2,6 +2,6 @@
 
 #set -x # debug
 
-PAR_ROOT=~/src/par              # where you downloaded par
+PAR_ROOT=~/code/par              # where you downloaded par
 export PYTHONPATH=$PAR_ROOT/lib # where par's Pyro is
 $PAR_ROOT/src/parallel.py "$@"  # call it
